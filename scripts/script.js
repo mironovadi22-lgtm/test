312dfac155001a6c3baa809de4a8ed2329e22aeb
mironovadi22-lgtm <mironovadi22@gmail.com>
2025-11-26 11:37:10 +0300

    сделала адаптив до 375px и бургер меню

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,36 @@
-const btn = document.getElementById("button")
-const links = document.querySelectorAll(".header__list-item-link")
-console.log(links)
-btn.addEventListener("click", func)
+// const btn = document.getElementById("button")
+// const links = document.querySelectorAll(".header__list-item-link")
+// console.log(links)
+// btn.addEventListener("click", func)
 
-function func() {
-   links.forEach((link) => {
-    link.style.color = 'red'
-   })
+// function func() {
+//    links.forEach((link) => {
+//     link.style.color = 'red'
+//    })
+// }
+
+// console.log(btn)
+
+const burgerButton = document.getElementById('burger-button')
+const burgerListMenu = document.getElementById('burger-list-menu')
+const burgerMenuItems = document.querySelectorAll('#burger-menu-item')
+const button = document.getElementById('button')
+burgerButton.addEventListener('click', burgerTrigger)
+const burgerMenuItemsArr = Array.from(burgerMenuItems)
+burgerMenuItemsArr.push(button)
+
+burgerMenuItemsArr.forEach(item => {
+   item.addEventListener('click', burgerTrigger)
+})
+
+let isBurgerMenuClose = true
+
+function burgerTrigger() {
+   if (isBurgerMenuClose) {
+      burgerButton.src = './images/Cross.svg'
+   } else {
+      burgerButton.src = './images/Hamburger.svg'
+   }
+   isBurgerMenuClose = !isBurgerMenuClose
+   burgerListMenu.classList.toggle("header__right_active")
 }
-
-console.log(btn)
